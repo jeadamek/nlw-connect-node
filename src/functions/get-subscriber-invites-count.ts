886414f88c549e9870081594998378802d651238
Jessica Adamek <jessica.g.adamek@gmail.com>
2025-02-24 20:03:50 -0300

    :sparkles: get subscriber ranking position route

--- src/functions/get-subscriber-invites-count.ts
+++ src/functions/get-subscriber-invites-count.ts
@@ -2,13 +2,13 @@ import { db } from '../drizzle/client'
 import { subscriptions } from '../drizzle/schema/subscriptions'
 import { redis } from '../redis/client'
 
-interface GetSubscriberInvitesCountsParams {
+interface GetSubscriberInvitesCountParams {
   subscriberId: string
 }
 
 export async function getSubscriberInvitesCount({
   subscriberId,
-}: GetSubscriberInvitesCountsParams) {
+}: GetSubscriberInvitesCountParams) {
   const count = await redis.zscore('referral:ranking', subscriberId)
 
   return { count: count ? Number.parseInt(count) : 0 }
